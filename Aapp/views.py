b3660a69fc641b75218a51ec2abffa6f3ab57299
slkom2 <slkom2@gmail.com>
2023-03-29 15:05:33 +0300

    добавил пагинацию

--- Aapp/views.py
+++ Aapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Joke
 
 def index(request):
     jokes = Joke.objects.all()
 
     # jokes = {'jokes': jokes}
-    return render(request, 'index.html', {'jokes': jokes})
+    # пагинация
+    paginator = Paginator(jokes, 2)
+    page_number = request.GET.get('page')
+    items = paginator.get_page(page_number)
+    return render(request, 'index.html', {'jokes': jokes, 'items': items})
 
 
 
